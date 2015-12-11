fb98ea450c4261a6b240b0cf680718256ee4f694
Noppes <noppes3000@gmail.com>
2015-12-11 16:37:34 +0100

    Some more recipe things

--- noppes/npcs/api/handler/IRecipeHandler.java
+++ noppes/npcs/api/handler/IRecipeHandler.java
@@ -2,6 +2,7 @@ package noppes.npcs.api.handler;
 
 import java.util.List;
 
+import net.minecraft.item.ItemStack;
 import noppes.npcs.api.handler.data.IRecipe;
 
 public interface IRecipeHandler {
@@ -9,6 +10,8 @@ public interface IRecipeHandler {
 	public List<IRecipe> getGlobalList();
 	
 	public List<IRecipe> getCarpentryList();
-	
-	public IRecipe delete(int id);
+
+	public IRecipe addRecipe(String name, boolean global, ItemStack result, Object... objects);
+
+	public IRecipe addRecipe(String name, boolean global, ItemStack result, int width, int height, ItemStack... recipe);
 }
--- noppes/npcs/api/handler/data/IRecipe.java
+++ noppes/npcs/api/handler/data/IRecipe.java
@@ -1,6 +1,40 @@
 package noppes.npcs.api.handler.data;
 
-public interface IRecipe {
+import net.minecraft.item.ItemStack;
 
-	public int getId();
+public interface IRecipe {
+	
+	public String getName();
+	
+	
+	public boolean isGlobal();
+	
+	public void setIsGlobal(boolean bo);
+	
+	public boolean getIgnoreNBT();
+	
+	public void setIgnoreNBT(boolean bo);
+	
+	public boolean getIgnoreDamage();
+	
+	public void setIgnoreDamage(boolean bo);
+	
+	public int getWidth();
+	
+	public int getHeight();
+	
+	public ItemStack getResult();
+	
+	public ItemStack[] getRecipe();
+	
+	/**
+	 * @param bo Whether or not the recipe saves with customnpcs recipes
+	 */
+	public void saves(boolean bo);
+	
+	public boolean saves();
+	
+	public void save();
+	
+	public void delete();
 }
