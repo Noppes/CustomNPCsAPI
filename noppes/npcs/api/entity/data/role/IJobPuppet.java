8ed9755636ae9b90a30d9872f1e423b859ba5566
Noppes <noppes3000@gmail.com>
2017-06-02 23:04:12 +0200

    new puppet stuff

--- noppes/npcs/api/entity/data/role/IJobPuppet.java
+++ noppes/npcs/api/entity/data/role/IJobPuppet.java
@@ -6,16 +6,34 @@ import noppes.npcs.api.entity.data.role.IJobPuppet.IJobPuppetPart;
 import noppes.npcs.api.event.NpcEvent;
 
 public interface IJobPuppet extends INPCJob{
+	
+	public boolean getIsAnimated();
+	
+	public void setIsAnimated(boolean bo);
+	
+	/**
+	 * @return (0-7)
+	 */
+	public int getAnimationSpeed();
+	
+	/**
+	 * @param speed (0-7)
+	 */
+	public void setAnimationSpeed(int speed);
 
 	/**
-	 * @param part 0:head, 1:left arm, 2:right arm, 3:body, 4:left leg, 5:right leg
+	 * Part 6-11 are for animation
+	 * @param part 0:head, 1:left arm, 2:right arm, 3:body, 4:left leg, 5:right leg, 6:head2, 7:left arm2, 8:right arm2, 9:body2, 10:left leg2, 11:right leg2
 	 * @return returns the part
 	 */
 	public IJobPuppetPart getPart(int part);
 
 	public static interface IJobPuppetPart{
+		
 		public int getRotationX();
+		
 		public int getRotationY();
+		
 		public int getRotationZ();
 		
 		public void setRotation(int x, int y, int z);
