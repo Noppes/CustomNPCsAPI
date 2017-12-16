47f53fde251ea03e2ada0d859e13e8951b6474dc
Noppes <noppes3000@gmail.com>
2017-12-16 21:36:24 +0100

    scripted item basics

--- noppes/npcs/api/constants/ItemType.java
+++ noppes/npcs/api/constants/ItemType.java
@@ -1,6 +1,6 @@
 package noppes.npcs.api.constants;
 /**
- * Animation Types
+ * Item Types
  */
 public class ItemType {
 	public static final int NORMAL = 0;
@@ -9,4 +9,5 @@ public class ItemType {
 	public static final int ARMOR = 3;
 	public static final int SWORD = 4;
 	public static final int SEEDS = 5;
+	public static final int SCRIPTED = 6;
 }
---
+++ noppes/npcs/api/item/IItemScripted.java
@@ -0,0 +1,10 @@
+package noppes.npcs.api.item;
+
+public interface IItemScripted extends IItemStack {
+	
+	public boolean hasTexture(int damage);
+
+	public String getTexture(int damage);
+	
+	public void setTexture(int damage, String texture);
+}
