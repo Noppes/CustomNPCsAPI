30fa4480268cecaff268eb890c150b01264bad70
Noppes <noppes3000@gmail.com>
2022-07-05 22:59:17 +0200

    - command fix - prevent npcs chasing indefinitely - container gui crash fix - IBlockScriptedDoor.executeCommand - visibility fix

--- noppes/npcs/api/block/IBlockScriptedDoor.java
+++ noppes/npcs/api/block/IBlockScriptedDoor.java
@@ -31,4 +31,13 @@ public interface IBlockScriptedDoor extends IBlock{
 	
 	public void setResistance(float resistance);
 
+	/**
+	 * On servers the enable-command-block option in the server.properties needs to be set to true <br>
+	 * Use /gamerule commandBlockOutput false/true to turn off/on command block feedback <br>
+	 * Setting NpcUseOpCommands to true in the CustomNPCs.cfg should allow the npc to run op commands, be warned this could be a major security risk, use at own risk <br>
+	 * For permission plugins the commands are run under uuid:c9c843f8-4cb1-4c82-aa61-e264291b7bd6 and name:[customnpcs]
+	 * @param command The command to be executed
+	 * @return Returns the commands output
+	 */
+	public String executeCommand(String command);
 }
