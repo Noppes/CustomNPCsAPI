db7ae4a19e6af05313b0a919ab6b524cc229d487
Karel <noppes3000@gmail.com>
2024-10-31 11:42:07 +0100

    - potion effect fix

--- noppes/npcs/api/constants/PotionEffectType.java
+++ noppes/npcs/api/constants/PotionEffectType.java
@@ -4,9 +4,7 @@ import net.minecraft.core.Registry;
 import net.minecraft.world.effect.MobEffect;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public final class PotionEffectType {
 	public static final int NONE = 0;
